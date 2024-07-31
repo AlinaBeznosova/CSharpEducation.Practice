ba5abe0f304d61565c18e2eef043ba2ca0587ef2
Alina Beznosova <abeznsva@gmail.com>
2024-07-31 16:34:07 +0400

    Практика 1. Задача 3

---
+++ Task1_3/3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class PracticeTask3
+{
+    static void Main(string[] args)
+    {
+       
+        Console.Write("Введите ваше имя ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Привет " + name + " !");
+
+    }
+}
