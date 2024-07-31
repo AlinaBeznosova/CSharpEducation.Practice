6f2bb567601ea6fc437b5f32751bb5f5716deafd
Alina Beznosova <abeznsva@gmail.com>
2024-07-31 16:34:39 +0400

    Практика 1. Задача 5

---
+++ Task1_5/5.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class PracticeTask5
+{
+    static void Main(string[] args)
+    {
+        const double Pi = 3.14;
+        int r = 14;
+        double S = Pi * r * r;
+
+        Console.WriteLine("Площадь круга с радиусом " + r + " см равен " +  S + " квадратных сантиметров");
+    }
+}
