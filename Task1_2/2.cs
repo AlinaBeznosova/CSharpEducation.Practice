14e6febccf7b4863e37dd913736dba5136872502
Alina Beznosova <abeznsva@gmail.com>
2024-07-31 16:33:03 +0400

    Практика 1. Задача 2

---
+++ Task1_2/2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PracticeTask2
+{
+    static void Main(string[] args)
+    {
+        int age = 27;
+        string name = "Алина";
+        float temperature = 20;
+        bool isFemale = true;
+
+        Console.WriteLine("Возраст: " + age);
+        Console.WriteLine("Имя: " + name);
+        Console.WriteLine("Температура за окном: " + temperature + " градусов");
+
+        if (isFemale == true)
+            Console.WriteLine("Пол: женский ");
+        else
+            Console.WriteLine("Пол: мужской ");
+    }
+}
