199563a1850bf0910016cc0499e7b3f85679eb1c
Alina Beznosova <abeznsva@gmail.com>
2024-07-31 16:34:29 +0400

    Практика 1. Задача 4

---
+++ Task1_4/4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class PracticeTask4
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первую фразу :");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("Введите вторую фразу :");
+        string second = Console.ReadLine();
+
+        
+        Console.WriteLine("Получившийся результат:" + first + " " + second);
+
+    }
+}
