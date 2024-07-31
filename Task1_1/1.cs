1f659fe011de03d20e8195db86dab3cded5a6b4a
Alina Beznosova <abeznsva@gmail.com>
2024-07-31 16:30:50 +0400

    Практическое задание 1

---
+++ Task1_1/1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class PracticeTask1
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            string companyName;
+            bool isUser;
+            float weight;
+         }
+    }
