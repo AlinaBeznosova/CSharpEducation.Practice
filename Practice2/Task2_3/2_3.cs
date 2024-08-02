e418b8f055e2386390478815e6dd3fa378809e57
Alina Beznosova <abeznsva@gmail.com>
2024-08-02 16:57:44 +0400

    Практика2. Задание3

---
+++ Practice2/Task2_3/2_3.cs
@@ -0,0 +1,41 @@
+﻿/*Напишите программу для вычисления выражения (а + b — f / а) + f * a * a — (a +
+b). Все переменные задаются пользователем. Учтите деление на 0.*/
+
+using System;
+
+class PracticeTask2_3
+{
+    static void Main()
+    {
+        Console.WriteLine("Посчитаем значение выражения (а + b — f / а) + f * a * a — (a + b)");
+        int a, b, f;
+        Console.Write("Введите переменную a, неравную 0: ");
+        
+       
+        string? userInput = Console.ReadLine();
+        a = Convert.ToInt32(userInput);
+
+        while (a == 0)
+        {
+            Console.Write("Вы ввели 0. Введите другое число ");
+            userInput = Console.ReadLine();
+            a = Convert.ToInt32(userInput);
+            if ( a != 0)
+                break;
+        }
+        
+
+        Console.Write("Введите переменную b: ");
+        userInput = Console.ReadLine();
+        b = Convert.ToInt32(userInput);
+
+        Console.Write("Введите переменную f: ");
+        userInput = Console.ReadLine();
+        f = Convert.ToInt32(userInput);
+
+        double result = (a + b - f / a) + f * a * a - (a + b);
+        Console.WriteLine("Значение выражения (а + b — f / а) + f * a * a — (a + b) = " + result);
+
+
+    }
+}
