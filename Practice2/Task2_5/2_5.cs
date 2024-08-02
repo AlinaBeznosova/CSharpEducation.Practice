595d76673a9c03ea756e8ae07b266b4c0bba4b8a
Alina Beznosova <abeznsva@gmail.com>
2024-08-02 16:58:42 +0400

    Практика2. Задание5

---
+++ Practice2/Task2_5/2_5.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу для сравнения двух целых чисел. Ввод осуществляйте
+через консоль*/
+
+using System;
+
+class PracticeTask2_5
+{
+    static void Main()
+    {
+        int a, b;
+
+        Console.Write("Введите первое число ");
+        string? input = Console.ReadLine();
+        a = Convert.ToInt32(input);
+
+        Console.Write("Введите второе число ");
+        input = Console.ReadLine();
+        b = Convert.ToInt32(input);
+
+        if (a > b)
+        {
+            Console.WriteLine($"{a} больше {b}");
+        }
+        else if (a < b)
+        {
+            Console.WriteLine($"{a} меньше {b}");
+        }
+        else if (a == b)
+        {
+            Console.WriteLine($"{a} равно {b}");
+        }
+    }
+}
