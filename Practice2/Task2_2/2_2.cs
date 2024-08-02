13ee6b2120d20018aba7762fdf912b26fa5a5a09
Alina Beznosova <abeznsva@gmail.com>
2024-08-02 16:57:14 +0400

    Практика 2. Задание2

---
+++ Practice2/Task2_2/2_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class PracticeTask2_2
+{
+    static void Main()
+    {
+        Console.Write("Введите число в метрах "); //метры в километры
+        string? userInput = Console.ReadLine();
+        double meters = Convert.ToDouble(userInput);
+        double kilometers = meters / 1000;
+        Console.WriteLine($"{meters} м равно {kilometers} км");
+
+
+        Console.Write("Введите число в километрах "); //километры в сантиметры
+        userInput = Console.ReadLine();
+        kilometers = Convert.ToDouble(userInput);
+        double santemeters = kilometers * 100000;
+        Console.WriteLine($"{kilometers} км равно {santemeters} см");
+
+
+        Console.Write("Введите скорость в м/с "); //м/с в км/ч
+        userInput = Console.ReadLine();
+        double speed_m = Convert.ToDouble(userInput);
+        double speed_km = speed_m * 3.6;
+        Console.WriteLine($"{speed_m} м/с равно {speed_km} км/ч");
+
+        Console.Write("Введите температуру в С "); //градусы C в градусы F
+        userInput = Console.ReadLine();
+        double temperatureC = Convert.ToDouble(userInput);
+        double temperatureF = ( temperatureC * 1.8) + 32;
+        Console.WriteLine($"{temperatureC} градусов по Цельсию эквивалентно {temperatureF} градусам по Фаренгейту");
+    }
+}
+       
