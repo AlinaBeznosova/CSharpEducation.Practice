3ecd1226ad0b5ec90e9e77b60c459a37af2c6867
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 17:53:13 +0400

    Практика2. Задание10

---
+++ Practice2/Task2_10/2_10.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, в которой создаётся массив и выводится на экран
+консоли. Размер массива передавайте в качестве первого аргумента командной
+строки.*/
+
+using System;
+
+class Practice2_10
+{
+   static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(args[0]);
+        int[] array = new int[n];  
+        Random rnd = new Random(); 
+
+        for(int i=0; i < n; i++)
+        {
+            array[i] = rnd.Next(1,100);
+            Console.Write(array[i] + " ");
+        }
+        Console.ReadLine();
+    }
+}
