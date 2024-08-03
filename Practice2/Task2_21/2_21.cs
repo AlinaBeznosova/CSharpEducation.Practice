f0bbe263629ae51f467f4ce6b3180fee55c7c3de
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 19:10:06 +0400

    Практика2. Задание21

---
+++ Practice2/Task2_21/2_21.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите игру Угадай число. Программа случайно генерирует число от 1 до
+100, а пользователь пытается угадать это число. При успешной догадке
+выводите поздравление пользователя.*/
+
+using System;
+class Practice2_21
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int mysteryNum = rand.Next(1,100);
+
+        Console.WriteLine("Компьютер загадал число от 1 до 100, а вам нужно его угадать");
+
+        while(true)
+        {
+            Console.Write("Введите число: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if(input == mysteryNum)
+            {
+                Console.WriteLine("Поздравляем! Вы угадали!");
+                break;
+            }
+            
+
+            else
+            {
+                Console.WriteLine("Упс! Неверно. Попробуйте еще раз");
+            }
+        }
+    }
+}
