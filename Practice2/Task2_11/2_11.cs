b13d7243f2e81b12526cb9675c8073b047e71ff6
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 17:53:31 +0400

    Практика2. Задание11

---
+++ Practice2/Task2_11/2_11.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, в которой создаётся массив и выводится на экран
+консоли. Размер массива передавайте в качестве первого аргумента командной
+строки. Число, которым будет заполняться массив передайте через второй
+аргумент командной строки*/
+
+using System;
+
+class Practice2_11
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(args[0]);
+        int[] array = new int[n];
+        int value = Convert.ToInt32(args[1]);
+
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = value;
+            Console.Write(array[i] + " ");
+        }
+
+        Console.ReadLine();
+    }
+}
