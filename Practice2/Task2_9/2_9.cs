ae1dc320d80d96b0e68eec9a8bb529adab63eaf6
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 17:52:31 +0400

    Практика2. Задание9

---
+++ Practice2/Task2_9/2_9.cs
@@ -0,0 +1,42 @@
+﻿/*Напишите метод, который создаёт массив целых чисел и возвращает его.
+Размер массива нужно передавать в качестве аргумента. Вдобавок напишите
+метод, который выводит переданный массив на экран консоли.*/
+
+using System;
+
+class PracticeTask2_9
+{
+     public static int[] createArray(int n)
+    {
+        int[] array = new int[n];
+        Random rand = new Random();
+        
+        for(int i = 0; i < n; i++) 
+        {
+           array[i] = rand.Next(1,20);
+        }
+        return array;
+        
+    }
+
+    public static void printArray(int[] array)
+    {
+        
+       for (int i = 0; i< array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+       Console.WriteLine();
+    }
+    static void Main()
+    {
+        Console.WriteLine("Введите размер массива:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+        arr = createArray(n);
+        printArray(arr);
+        
+
+    }
+}
