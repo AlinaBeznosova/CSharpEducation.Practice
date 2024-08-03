537636c51581188a38812d7256183c72924fbd46
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 14:11:40 +0400

    Практика2. Задание7

---
+++ Practice2/Task2_7/2_7.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая переводит строку в разные регистры
+
+using System;
+
+class PracticeTask2_7
+{
+    static void Main(string[] args)
+    {
+       // a.в верхний регистр: f->F
+        Console.WriteLine($"Введите строку 1: ");
+        string? str = Console.ReadLine();
+        string upperStr = str.ToUpper();
+        Console.WriteLine($"Строка в верхнем регистре: {upperStr}");
+
+        //b.в нижний регистр: F->f
+        Console.WriteLine($"Введите строку 2: ");
+        str = Console.ReadLine();
+        string downStr = str.ToLower();
+        Console.WriteLine($"Строка в нижнем регистре: {downStr}");
+
+        //c. делает заглавную букву в слове: привет -> Привет
+        Console.WriteLine($"Введите строку 3: ");
+        str = Console.ReadLine();
+        string firstLetter = str.Substring(0, 1).ToUpper() + str.Substring(1).ToLower();
+        Console.WriteLine($"Строка с заглавной буквы: {firstLetter}");
+
+    }
+}
