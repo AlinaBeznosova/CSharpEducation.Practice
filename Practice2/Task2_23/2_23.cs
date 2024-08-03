28a13b7fb772a5ebf1cab055d6207c9bc7753f27
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 19:10:45 +0400

    Практика2. Задание23

---
+++ Practice2/Task2_23/2_23.cs
@@ -0,0 +1,29 @@
+﻿/*Напишите программу для вычисления високосного года.
+a. год, номер которого кратен 400, — високосный;
+остальные годы, номер которых кратен 100, — невисокосные
+остальные годы, номер которых кратен 4, — високосный;
+все остальные годы — невисокосные.*/
+
+using System;
+class Practice2_23
+{
+    public static bool checkYear(int year)
+    {
+        return (year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0));
+    }
+    static void Main()
+    {
+        Console.WriteLine("Давайте выясним, является ли год високосным. Введите год:");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        if (checkYear(year))
+        {
+            Console.WriteLine("Год високосный");
+        }   
+        else
+        {
+            Console.WriteLine("Год невисокосный");
+        }
+    }
+
+}
