4adad2391140dcd1eb423b4d5324599027644052
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 19:11:08 +0400

    Практика2. Задание24

---
+++ Practice2/Task2_24/2_24.cs
@@ -0,0 +1,24 @@
+﻿/*Создайте перечисление(enum) для дней недели. Напишите программу, которая
+выводит на экран дни недели по названиям*/
+
+using System;
+class Practice2_24
+{
+    public enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wensday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    static void Main()
+    {
+        foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+        {
+            Console.WriteLine(day);
+        }
+    }
+}
