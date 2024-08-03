df559b64b26e08fb3664dd4dd99efe2200515b0b
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 17:53:57 +0400

    Практика2. Задание12

---
+++ Practice2/Task2_12/2_12.cs
@@ -0,0 +1,28 @@
+﻿/* Напишите программу, в которой создаётся массив строк, который заполняется
+пользователем через консоль. Затем этот массив должен быть выведен на
+экран консоли*/
+
+using System;
+
+class Practice2_12
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите размер массива: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] array = new string[n];
+
+        Console.WriteLine("Заполните массив строками");
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Введите {i+1} строку:");
+            array[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Результат:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
