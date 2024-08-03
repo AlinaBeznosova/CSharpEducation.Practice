b32a7b636aa465797bfc8dc5c9b0d3c42bab0639
Alina Beznosova <abeznsva@gmail.com>
2024-08-03 17:55:33 +0400

    Практика2. Задание16

---
+++ Practice2/Task2_16/2_16.cs
@@ -0,0 +1,21 @@
+﻿/*Создайте и реализуйте метод, который будет принимать число по ссылке (ref) и
+менять его знак на противоположный. Метод должен принимать один аргумент и
+не возвращать ничего.*/
+
+using System;
+class Practice2_16
+{
+    public static void changeSign (ref int n)
+    {
+        n = -n;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        changeSign(ref n);
+        Console.WriteLine($"Новое число : {n}");
+    }
+}
