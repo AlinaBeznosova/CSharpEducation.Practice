586813e855eabf3fe3c1b176581caa75848950e7
Alina Beznosova <abeznsva@gmail.com>
2024-08-07 20:59:40 +0400

    Практика3. Задание2

---
+++ Practice3/Task3_2/3_2.cs
@@ -0,0 +1,29 @@
+﻿/*Создай класс "Студент" с полями "Имя" и "Возраст". Напиши метод, который будет
+выводить информацию о студенте в консоль. Добавь в класс "Студент" свойство
+"Средний балл". Реализуй геттер и сеттер для этого свойства. Добавь проверку в
+сеттер, чтобы значение среднего балла было в диапазоне от 0 до 5.*/
+
+using System;
+using Task3_2;
+
+namespace Task3_2
+{
+    class Practice3_1
+    {
+
+        static void Main()
+        {
+            Student Alina = new Student();
+            int studentAge = 27;
+            string studentName = "Alina";
+            double mark = 4.7;
+
+            Alina.Name = studentName;
+            Alina.Age = studentAge;
+            Alina.Average = mark;
+
+            Alina.printInfo();
+
+        }
+    }
+}
---
+++ Practice3/Task3_2/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_2
+{
+    class Student
+    {
+        private string name;
+        private int age;
+        private double average;
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if ((this.age < 0) || (this.age > 120))
+                    Console.WriteLine("Некорректный ввод возраста");
+                else
+                    this.age = value;
+            }
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine($"Имя : {name}");
+            Console.WriteLine($"Возраст : {age}");
+            Console.WriteLine($"Средний балл : {average}");
+        }
+
+        public double Average
+        {
+            get { return this.average; }
+
+            set
+            {
+                if (this.average < 0 || this.average > 5)
+                    Console.WriteLine("Средний балл должен быть в диапазоне от 0 до 5");
+                else { this.average = value; }
+            }
+
+        }
+
+    }
+}
