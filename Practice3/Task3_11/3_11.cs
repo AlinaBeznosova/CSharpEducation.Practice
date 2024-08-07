23fa417c22117da4718aba06754881d5699aba40
Alina Beznosova <abeznsva@gmail.com>
2024-08-07 21:03:42 +0400

    Практика3. Задание11

---
+++ Practice3/Task3_11/3_11.cs
@@ -0,0 +1,24 @@
+﻿/*Создай класс "Книга" с полями "Название" и "Автор". Напиши метод, который будет
+принимать объект класса "Книга" и возвращать информацию о книге в виде строки.*/
+
+using System;
+using Task3_11;
+
+class Practice3_11
+{
+    public static void printInfo(Book book)
+    {
+        Console.WriteLine($"Название книги: {book.nameBook} Автор: {book.nameAuthor} ");
+    }
+    static void Main()
+    {
+        Book book1 = new Book();
+        Book book2 = new Book("Сияние");
+        Book book3 = new Book("Куджо", "Стивен Кинг");
+
+        printInfo(book1);
+        printInfo(book2);
+        printInfo(book3);   
+
+    }
+}
---
+++ Practice3/Task3_11/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_11
+{
+    class Book
+    {
+        public string nameBook;
+        public string nameAuthor;
+
+        public Book() : this("Неизвестно")
+        { }
+
+        public Book(string nameBook) : this(nameBook, "Неизвестный")
+        { }
+
+        public Book(string nameBook, string nameAuthor)
+        {
+            this.nameBook = nameBook;
+            this.nameAuthor = nameAuthor;
+
+        }
+
+    }
+}
