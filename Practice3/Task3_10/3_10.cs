39ba95e3fcde211fc9c6dfa0bc74fbf323212c0e
Alina Beznosova <abeznsva@gmail.com>
2024-08-07 21:03:21 +0400

    Практика3. Задание10

---
+++ Practice3/Task3_10/3_10.cs
@@ -0,0 +1,18 @@
+﻿/*Создай класс "Автомобиль" с полем "Марка". Напиши метод, который будет
+принимать объект класса "Автомобиль" и выводить информацию о марке
+автомобиля в консоль.*/
+
+using System;
+using Task3_10;
+class Practice3_10
+{
+    public static void printModel(Auto car)
+    {
+        Console.WriteLine($"Марка автомобиля {car.model}");
+    }
+    static void Main()
+    {
+        Auto car = new Auto("BMW");
+        printModel(car);
+    }
+}
---
+++ Practice3/Task3_10/Auto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_10
+{
+    class Auto
+    {
+        public string model;
+
+        public Auto(string model)
+        {
+            this.model = model;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+    }
+}
