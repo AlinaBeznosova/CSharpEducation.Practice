f6b85b3bd83800b76d0d4aca9f96073584db0f17
Alina Beznosova <abeznsva@gmail.com>
2024-08-07 21:02:46 +0400

    Практика3. Задание9

---
+++ Practice3/Task3_9/3_9.cs
@@ -0,0 +1,25 @@
+﻿/*Создай класс "Студент" с полями "Имя" и "Возраст". Напиши метод, который будет
+принимать объект класса "Студент" и изменять его имя на "Аноним".*/
+
+using System;
+using System.Xml.Linq;
+using Task3_9;
+
+class Practice3_9
+{
+    public static void СhangeNameStudent(Student student)
+    {
+        student.Name = "Аноним";
+    }
+    static void Main()
+    {
+        Student alina = new Student();
+        alina.Name = "Алина";
+        alina.Age = 27;
+        alina.printInfo();
+
+        СhangeNameStudent(alina);
+        alina.printInfo();
+    }  
+
+}
---
+++ Practice3/Task3_9/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_9
+{
+    class Student
+    {
+        public string name;
+        public int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if ((this.age < 0) || (this.age > 120))
+                    Console.WriteLine("Некорректный ввод возраста");
+                else
+                    this.age = value;
+            }
+        }
+        public void printInfo()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+
+        
+    }
+}
+
