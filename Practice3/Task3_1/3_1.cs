5ed87c5977a2f58af5a6dcfd400d060b3f3e34e7
Alina Beznosova <abeznsva@gmail.com>
2024-08-07 20:51:06 +0400

    Практика3. Задание1

---
+++ Practice3/Task3_1/3_1.cs
@@ -0,0 +1,24 @@
+﻿/*Создай класс "Студент" с полями "Имя" и "Возраст". Напиши метод, который будет
+выводить информацию о студенте в консоль.*/
+
+using System;
+
+namespace Task3_1
+{
+    class Practice3_1
+    {
+
+        static void Main()
+        {
+            Student Alina = new Student();
+            string studentName = Alina.name;
+            Alina.name = "Alina";
+
+            int studentAge = Alina.age;
+            Alina.age = 27;
+
+            Alina.printInfo();
+
+        }
+    }
+}
---
+++ Practice3/Task3_1/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_1
+{
+    internal class Student
+    {
+        public string name;
+        public int age;
+
+        public void printInfo()
+        {
+            Console.WriteLine($"Имя : {name} Возраст {age}");
+        }
+    }
+}
