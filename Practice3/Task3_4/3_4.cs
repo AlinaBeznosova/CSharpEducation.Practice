0fa2fec6fde15914bfbe3f52df75cd54bd259b55
Alina Beznosova <abeznsva@gmail.com>
2024-08-07 21:00:32 +0400

    Практика3. Задание4

---
+++ Practice3/Task3_4/3_4.cs
@@ -0,0 +1,20 @@
+﻿/*Создай класс "Книга" с полями "Название" и "Автор". Реализуй два конструктора:
+один с параметрами для инициализации полей, другой без параметров, который
+будет устанавливать значения по умолчанию.*/
+
+using System;
+using Task3_4;
+
+class Practice3_4
+{
+    static void Main()
+    {
+        Book book1 = new Book();
+        Console.WriteLine("Книга 1 ");
+        book1.printInfo();
+
+        Book book2 = new Book("Сияние", "Стивен Кинг");
+        Console.WriteLine("Книга 2 ");
+        book2.printInfo();
+    }
+}
---
+++ Practice3/Task3_4/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task3_4
+{
+    class Book
+    {
+        private string nameBook;
+        private string nameAuthor;
+
+        public Book() : this ("Неизвестно","Неизвестный")
+        { }
+        public Book(string nameBook, string nameAuthor)
+        {
+            this.nameBook = nameBook;
+            this.nameAuthor = nameAuthor;
+
+        }
+        public void printInfo()
+        {
+            Console.WriteLine($"Название : {nameBook} ");
+            Console.WriteLine($"Автор :{nameAuthor}");
+        }
+    }
+}
