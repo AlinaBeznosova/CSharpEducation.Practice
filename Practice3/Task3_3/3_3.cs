70a317fab9b398ce2dd81f9b309f757ee55127db
Alina Beznosova <abeznsva@gmail.com>
2024-08-07 21:00:04 +0400

    Практика3. Задание3

---
+++ Practice3/Task3_3/3_3.cs
@@ -0,0 +1,19 @@
+﻿/*Создай класс "Калькулятор" с методами для выполнения основных арифметических
+операций (сложение, вычитание, умножение, деление). Пусть эти методы
+принимают два числа и возвращают результат операции.*/
+
+using System;
+
+namespace Task3_3
+{
+    class Practice3_3
+    {
+        static void Main()
+        {
+            Console.WriteLine($"5 + 7 = {Calculator.Add(5, 7)}");
+            Console.WriteLine($"10 - 3 = {Calculator.Subtract(10, 3)}");
+            Console.WriteLine($"8 * 4 = {Calculator.Multiply(8, 4)}");
+            Console.WriteLine($"27 / 9 = {Calculator.Devide(27, 9)}");
+        }
+    }
+}
---
+++ Practice3/Task3_3/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_3
+{
+    class Calculator
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Subtract(int x, int y) => x - y;
+        public static int Multiply(int x, int y) => x * y;
+        public static double Devide(double x, double y) =>  x / y;
+        
+    }
+}
