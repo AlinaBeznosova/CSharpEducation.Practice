d1511d88048384d3cdbfb25d2185bc113e94245d
Alina Beznosova <abeznsva@gmail.com>
2024-08-14 17:34:07 +0400

    Практика 3, часть 2, библиотека классов MyLibPerson, в ней класс Person

---
+++ MyLibPerson/Person.cs
@@ -0,0 +1,34 @@
+﻿namespace MyLibPerson
+{
+    public class Person
+    {
+        string? name;
+        int age;
+        public string? Name
+        {
+            get => this.name;
+            set
+            {
+                if (value == null)
+                    Console.WriteLine($"Не корректный ввод. Не бывает людей без имени.");
+                else this.name = value;
+            }
+        }
+        
+
+        public int Age
+        {
+            get => this.age;
+
+            set
+            {
+                if (value < 0 || value > 120)
+                    Console.WriteLine($"Некорректный возраст. ");
+                else this.age = value;
+            }
+        }
+        
+        
+        public void PrintInfo() => Console.WriteLine($"Имя: {name} , Возраст: {age}");
+    }
+}
---
+++ Practice3/3_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyLibPerson;
+
+class Program
+{
+    static void Main()
+    {
+        Person Alina = new Person();
+        Alina.Name = "Алина";
+        Alina.Age = 28;
+
+        Alina.PrintInfo();
+    }
+}
+
+
