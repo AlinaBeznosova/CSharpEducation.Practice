1e2106a86834b86aed5bca9d1cdc6e18266b14e4
Alina Beznosova <abeznsva@gmail.com>
2024-08-14 17:32:54 +0400

    Практика 3, часть 2. Добавила иблиотеку классов MyLib и в ней класс MathHelper

---
+++ MyLib/MathHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace MyLib
+{
+    public class MathHelper
+    {
+        private static double result;
+        public static void Addition(double x, double y)
+        {
+            Console.WriteLine($"Результат сложения равен {result = x + y}");
+        }
+
+        public static void Substraction(double x, double y)
+        {
+            Console.WriteLine($"Результат вычитания равен {result = x - y}");
+        }
+
+        public static void Multiplication(double x, double y)
+        {
+            Console.WriteLine($"Результат умножения равен {result = x * y}");
+        }
+
+        public static void Division(double x, double y)
+        {
+            if (y == 0)
+            {
+               Console.WriteLine($"На ноль делить нельзя");
+            }
+            else Console.WriteLine($"Результат деления равен {result = x / y}");
+        }
+    }
+}
+ 
---
+++ Practice3/3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MyLib;
+
+class Program
+{
+    static void Main()
+    {
+        double a;
+        double b;
+
+        Console.WriteLine($"Введите два числа для сложения: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        b = Convert.ToDouble(Console.ReadLine());
+
+        MathHelper.Addition(a, b);
+
+        Console.WriteLine($"Введите два числа для вычитания: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        b = Convert.ToDouble(Console.ReadLine());
+
+        MathHelper.Substraction(a, b);
+
+        Console.WriteLine($"Введите два числа для умножения: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        b = Convert.ToDouble(Console.ReadLine());
+
+        MathHelper.Multiplication(a, b);
+
+        Console.WriteLine($"Введите два числа для деления: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        b = Convert.ToDouble(Console.ReadLine());
+
+        MathHelper.Division(a, b);
+    }
+}
+
