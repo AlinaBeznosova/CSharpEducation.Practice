dcb8d370211b0bb820f1d02d6d461779acbbedbc
Alina Beznosova <abeznsva@gmail.com>
2024-09-16 20:08:42 +0400

    Практика 5, задание 1.

---
+++ Practice5/Practice5/Manager.cs
@@ -0,0 +1,74 @@
+﻿namespace Practice5
+{
+	/// <summary>
+	/// Класс менеджер.
+	/// </summary>
+	class Manager : Employee
+	{
+		#region Поля.
+
+		/// <summary>
+		/// Размер команды.
+		/// </summary>
+		private int TeamSize;
+
+		#endregion
+
+		#region Методы.
+
+		/// <summary>
+		/// Вывод информации о менеджере в консоль.
+		/// </summary>
+		public override void PrintInfo()
+		{
+			base.PrintInfo();
+			Console.WriteLine($"Команда {TeamSize} человек");
+		}
+
+		/// <summary>
+		/// Вычисление бонуса для менеджера.
+		/// </summary>
+		public override void CalculateBonus()
+		{
+			double bonus = Salary * 0.2;
+			if (TeamSize == 0)
+			{
+				Console.WriteLine($"Бонус менеджера {name} с зарплатой {salary} равен {bonus}");
+				return;
+			}
+			else if (TeamSize > 5)
+			{
+				bonus += bonus * 0.05;
+			}
+			Console.WriteLine($"Бонус менеджера {name} с командой из {TeamSize} человек и зарплатой {salary} равен {bonus}");
+
+		}
+
+		#endregion
+
+		#region Конструкторы.
+
+		/// <summary>
+		/// Конструктор.
+		/// </summary>
+		/// <param name="name">Имя менеджера.</param>
+		/// <param name="salary">Зарпалата менеджера.</param>
+		public Manager(string name, double salary)
+				: base(name, salary)
+		{ }
+
+		/// <summary>
+		/// Конструктор.
+		/// </summary>
+		/// <param name="name">Имя менеджера.</param>
+		/// <param name="salary">Зарпалата менеджера.</param>
+		/// <param name="teamSize">Размер команды.</param>
+		public Manager(string name, double salary, int teamSize)
+				: base(name, salary)
+		{
+			TeamSize = teamSize;
+		}
+
+		#endregion
+	}
+}
---
+++ Practice5/Practice5/Program.cs
@@ -0,0 +1,40 @@
+﻿using Practice5;
+
+class Program
+{
+	static void Main()
+	{
+		Employee doctor = new Employee("Alex", 100000);
+		Manager tomManager = new Manager("Tom", 130000);
+
+		Console.Write("Введите имя учителя ");
+		string name = Console.ReadLine();
+		Console.Write("Введите зарплату учителя ");
+		double salary = Convert.ToDouble(Console.ReadLine());
+
+		Employee teacher = new Employee(name, salary);
+
+		Manager samManager = new Manager("Sam", 80000, 4);
+
+		Manager kateManager = new Manager("Kate", 80000, 6);
+
+		Contractor bobContractor = new Contractor("Bob", 140000, 500);
+
+		int hourWorked = 100;
+
+
+		List<Employee> employees = new List<Employee> { doctor, tomManager, teacher, samManager, kateManager, bobContractor };
+
+		foreach (var emp in employees)
+		{
+			if (emp is Contractor contractor)
+			{
+				contractor.CalculateBonus(hourWorked);
+			}
+			else
+			{
+				emp.CalculateBonus();
+			}
+		}
+	}
+}
