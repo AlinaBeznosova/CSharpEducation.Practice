3bfba7e22e3d361716727a1f6f249774e24ff97c
Alina Beznosova <abeznsva@gmail.com>
2024-09-19 17:37:53 +0400

    Практика 5, задание 3

---
+++ Practice5/Practice5_3/Cat.cs
@@ -0,0 +1,42 @@
+﻿namespace Practice5_3
+{
+	/// <summary>
+	/// Класс кот.
+	/// </summary>
+	class Cat : Animal
+	{
+		#region Методы
+
+		/// <summary>
+		/// Переопределенный метод какой звук издает кот.
+		/// </summary>
+		public override void MakeSound()
+		{
+			Console.WriteLine("Meow!");
+		}
+
+		/// <summary>
+		/// Переопределенный метод вывод информации о коте.
+		/// </summary>
+		public override void PrintInfo()
+		{
+			Console.WriteLine($"Name of cat {name} , age {age}");
+		}
+
+		#endregion
+
+		#region Конструкторы
+
+		/// <summary>
+		/// Конструктор базового класса.
+		/// </summary>
+		/// <param name="name">Имя кота.</param>
+		/// <param name="age">Возраст кота.</param>
+		public Cat(string name, int age) : base(name, age)
+		{
+			Console.WriteLine("Cat is created");
+		}
+
+		#endregion
+	}
+}
---
+++ Practice5/Practice5_3/Dog.cs
@@ -0,0 +1,42 @@
+﻿namespace Practice5_3
+{
+	/// <summary>
+	/// Класс собака.
+	/// </summary>
+	class Dog : Animal
+	{
+		#region Методы
+
+		/// <summary>
+		/// Переопределнный метод какой хвук издает собака.
+		/// </summary>
+		public override void MakeSound()
+		{
+			Console.WriteLine("Woof!");
+		}
+
+		/// <summary>
+		/// Переопределенный метод вывод информации о собаке..
+		/// </summary>
+		public override void PrintInfo()
+		{
+			Console.WriteLine($"Name of dog {name} , age {age}");
+		}
+
+		#endregion
+
+		#region Конструкторы
+
+		/// <summary>
+		/// Базовый конструктор.
+		/// </summary>
+		/// <param name="name">Имя собаки.</param>
+		/// <param name="age">Возраст собаки.</param>
+		public Dog(string name, int age) : base(name, age)
+		{
+			Console.WriteLine("Dog is created");
+		}
+
+		#endregion
+	}
+}
